607153fb5713095ff72da1cc47b9496773627871
Danny Wade <danny.lee.wade@gmail.com>
2022-03-22 21:29:11 +0000

    Initial Commit

---
+++ GolfStablefordCalculator/GolfStablefordCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfStablefordCalculatorApp.swift
+//  GolfStablefordCalculator
+//
+//  Created by Danny Wade on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GolfStablefordCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
