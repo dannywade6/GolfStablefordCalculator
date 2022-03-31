81bd1506c534ac74aa13e0eefcdfc95927546b68
Danny Wade <danny.lee.wade@gmail.com>
2022-03-31 12:54:30 +0100

    Add contents to ScoreListView.swift using a Navigation View to organise a list of rounds.

--- GolfStablefordCalculator/Views/ScoreListView.swift
+++ GolfStablefordCalculator/Views/ScoreListView.swift
@@ -7,30 +7,71 @@
 
 import SwiftUI
 
-struct ScoreListView: View {
+struct Round: Identifiable {
+    var id = UUID()
+    var course: String
+    var date: String
+    var points: Int
+}
 
-    @State var rounds = ["Augusta", "Pebble Beach", "St Andrews", "Royal Troon"]
-
+struct RoundRow: View {
+    var round: Round
+    
     var body: some View {
-        List {
-            ForEach(rounds, id: \.self) { round in
-
-                HStack {
-
-                    Text(round)
-                    Spacer()
-                    Text("03/03/2022")
-                        .fontWeight(.light)
-                    
-                    Text("36 Points")
-                        .fontWeight(.bold)
-                }
-
-
+        HStack {
+            VStack(alignment: .leading) {
+                Text(round.course)
+                Text(round.date)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
             }
+            Spacer()
+            Text("\(round.points) Points")
         }
     }
 }
+
+struct ScoreListView: View {
+    
+    let previousRounds = [
+    Round(course: "Augusta", date: "20/03/2022", points: 31),
+    Round(course: "Pebble Beach", date: "23/03/2022", points: 35),
+    Round(course: "St Andrews", date: "27/03/2022", points: 27),
+    Round(course: "Royal Troon", date: "31/03/2022", points: 36),
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List(previousRounds) { round in
+                NavigationLink(destination: DetailView(round: round)) {
+                RoundRow(round: round)
+                }
+            }
+            .navigationBarTitle("My Rounds")
+        }
+        
+    }
+}
+
+struct DetailView: View {
+    
+    var round: Round
+    
+    var body: some View {
+        VStack {
+            Text(round.course)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 3)
+
+            HStack {
+                Text("\(round.date) - \(round.points) Points.")
+            }
+            Spacer()
+        }
+    }
+}
+
 
 struct ScoreListView_Previews: PreviewProvider {
     static var previews: some View {
